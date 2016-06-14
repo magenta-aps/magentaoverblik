f6fc198dc902d84eeffa73db7c0fad0f55d24311
Gunnar H. Heinesen <gunnar@magenta.dk>
2016-06-14 14:25:59 +0200

    Implemented login.

--- overblik/urls.py
+++ overblik/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
+from overblik.views import IndexView, LoginView
 
 urlpatterns = [
-
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^login/$', LoginView.as_view(), name='login'),
 ]
